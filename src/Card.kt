42c93faedb49519c16d46a997626fca8f83e8442
Saem <ottosigma@gmail.com>
2016-09-02 00:26:10 +0300

    Tableau pile, code refactoring

--- src/Card.kt
+++ src/Card.kt
@@ -1,1 +1,10 @@
-class Card(val value: Int, val suit: String, var faceUp: Boolean = false)
+val clubs = "Clubs"
+val diamonds = "Diamonds"
+val hearts = "Hearths"
+val spades = "Spades"
+val redSuits = arrayOf(diamonds, hearts)
+val blackSuits = arrayOf(clubs, spades)
+
+class Card(val value: Int, val suit: String, var faceUp: Boolean = false) {
+
+}
